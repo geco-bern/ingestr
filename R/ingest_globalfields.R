a438a5228e73e77249a1272cba578d552fa0a1ca
stineb <b.stocker@imperial.ac.uk>
2020-03-12 20:16:11 +0100

    removed separate functions again

--- R/ingest_globalfields.R
+++ R/ingest_globalfields.R
@@ -429,3 +429,268 @@ extract_pointdata_allsites <- function( filename, df_lonlat, get_time = FALSE ){
   
   return(df_lonlat)
 }
+
+#' Implements a weather generator
+#'
+#' Implements a weather generator to simulate daily precipitation, given the monthly total and the number of days with rain for each month.
+#' 
+#' @param mval_prec A vector of twelve numeric values for monthly values of total precipitation.
+#' @param mval_wet A vector of twelve integer values for the number of wet days in each month.
+#' @param set_seed A logical specifying whether a random seed is set.
+#' @param leapyear A logical specifying whether interpolation is done for a leap year (with 366 days).
+#'
+#' @return A named list of data frames (tibbles) containing input data for each site is returned.
+#' 
+get_daily_prec <- function( mval_prec, mval_wet, set_seed=FALSE, leapyear=FALSE ){
+  #--------------------------------------------------------------------
+  # Distributes monthly total precipitation to days, given number of 
+  # monthly wet days. Adopted from LPX.
+  #--------------------------------------------------------------------
+  if (leapyear){
+    ndaymonth <- c(31,29,31,30,31,30,31,31,30,31,30,31)
+  } else {
+    ndaymonth <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+  }
+  ndayyear <- sum(ndaymonth)
+  nmonth <- length(ndaymonth)
+  
+  c1 <- 1.0
+  c2 <- 1.2
+  
+  if (set_seed) {set.seed(0)}
+  prdaily_random <- array( NA, dim=c(ndayyear,2))
+  for (doy in 1:ndayyear){
+    prdaily_random[doy,] <- runif(2)
+  }
+  
+  dval_prec <- rep(NA,ndayyear)
+  doy <- 0
+  prob <- 0.0
+  prob_rain <- rep(NA,nmonth)
+  mprecave <- rep(NA,nmonth)
+  mprecip <- rep(NA,nmonth)
+  for (moy in 1:nmonth){
+    prob_rain[moy] <- 0.0
+    mprecave[moy] <- 0.0
+    mprecip[moy] <- 0.0      
+  }
+  daysum <- 0
+  
+  set.seed( prdaily_random[1,1] * 1e7 )
+  
+  for (moy in 1:nmonth){
+    if ( mval_wet[moy]<=1.0 ) {mval_wet[moy] <- 1.0}
+    prob_rain[moy] <- mval_wet[moy] / ndaymonth[moy]
+    mprecave[moy] <- mval_prec[moy] / mval_wet[moy]
+    dry <- TRUE
+    iloop <- 0
+    
+    
+    while( dry ){
+      iloop <- iloop + 1
+      nwet <- 0
+      for (dm in 1:ndaymonth[moy]){
+        doy <- doy + 1
+        
+        # Transitional probabilities (Geng et al. 1986)
+        if (doy>1) {
+          if (dval_prec[doy-1] < 0.1) {
+            prob <- 0.75 * prob_rain[moy]
+          } else { 
+            prob <- 0.25 + (0.75 * prob_rain[moy])
+          }
+        }        
+        # Determine we randomly and use Krysanova / Cramer estimates of 
+        # parameter values (c1,c2) for an exponential distribution
+        if (iloop==1) { 
+          vv <- prdaily_random[doy,1]
+        } else {
+          # xxx problem: rand() generates a random number that leads to floating point exception
+          vv <- runif(1)
+        }
+        
+        
+        if (vv>prob) {        
+          dval_prec[doy] <- 0.0
+        } else {        
+          nwet <- nwet + 1        
+          v1 <- prdaily_random[doy,2]        
+          dval_prec[doy] <- ((-log(v1))^c2) * mprecave[moy] * c1         
+          if (dval_prec[doy] < 0.1) dval_prec[doy] <- 0.0        
+        }
+        
+        mprecip[moy] <- mprecip[moy] + dval_prec[doy]
+      }
+      
+      # If it never rained this month and mprec[moy]>0 and mval_wet[moy]>0, do
+      # again
+      dry <- (nwet==0 && iloop<50 && mval_prec[moy]>0.1)
+      if (iloop>50) {
+        print('Daily.F, prdaily: Warning stopped after 50 tries in cell')
+      }
+      
+      # Reset counter to start of month          
+      if (dry) {
+        doy <- doy - ndaymonth[moy]
+      }
+      
+    } #while
+    
+    # normalise generated precipitation by monthly CRU values
+    if ( moy > 1 ) {daysum <- daysum + ndaymonth[moy-1]}
+    if ( mprecip[moy] < 1.0 ) {mprecip[moy] <- 1.0}
+    for (dm in 1:ndaymonth[moy]){
+      doy <- daysum + dm
+      dval_prec[doy] <- dval_prec[doy] * (mval_prec[moy] / mprecip[moy])
+      if ( dval_prec[doy] < 0.1 ) {dval_prec[doy] <- 0.0}
+      # dval_prec[doy] <- mval_prec[moy] / ndaymonth[moy]  #no generator
+    }
+    
+    # Alternative: equal distribution of rain for fixed number of wet days
+    # prob <- prob_rain[moy] + prob
+    # if (prob.ge.1.0) then   
+    #   dval_prec[doy] <- mprec[moy]
+    #   prob <- prob-1.0
+    # } else {
+    #   dval_prec[doy] <- 0.0
+    #   prob <- prob
+    # }
+    
+  } 
+  
+  return( dval_prec )
+  
+}
+
+
+#' Interpolates monthly to daily values
+#'
+#' Implements different methods to interpolate from monthly to daily values, including fitting a polynomial.
+#' 
+#' @param mval A vector of twelve numeric values for monthly values.
+#' @param method A character string specifying the method for interpolation. Defaults to \code{"polynom"} for using a polynomial.
+#' @param mval_prev The monthly value of the month before the twelve months for which values are provided by argument \code{mval}.
+#' @param mval_next The monthly value of the month after the twelve months for which values are provided by argument \code{mval}.
+#' @param leapyear A logical specifying whether interpolation is done for a leap year (with 366 days).
+#'
+#' @return A named list of data frames (tibbles) containing input data for each site is returned.
+#' 
+monthly2daily <- function( mval, method="polynom", mval_prev=mval[nmonth], mval_next=mval[1], leapyear=FALSE ){
+  
+  # mval <- 20*sin( seq(0, 2*pi, 2*pi/11)-0.5*pi)
+  # mval_prev <- mval[12]
+  # mval_next <- mval[1]
+  
+  if (leapyear){
+    ndaymonth <- c(31,29,31,30,31,30,31,31,30,31,30,31)
+  } else {
+    ndaymonth <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+  }
+  nmonth <- length(ndaymonth)
+  dval <- rep(NA,sum(ndaymonth))
+  
+  if (method=="polynom"){
+    
+    # Starting conditons of december in previous year
+    startt <- -30.5            # midpoint between Nov-Dec of previous year
+    endt <- 0.5                # midpoint between Dec-Jan of this year
+    dt <- 31.0                 # number of Dec days
+    lastmonthtemp <- mval_prev # Dec mean temperature
+    day <- 0                   # initialisation of this years days
+    
+    for (month in 1:nmonth){
+      dtold <- dt
+      dt <- (ndaymonth[month])
+      startt <- endt
+      endt <- endt + dt
+      if (month<nmonth) {
+        dtnew <- (ndaymonth[month+1])
+        nextmonthtemp <- mval[month+1]
+      } else {
+        dtnew <- (ndaymonth[1])
+        nextmonthtemp <- mval_next
+      }
+      
+      starttemp <- (mval[month]*dt+lastmonthtemp*dtold)/(dt+dtold)
+      endtemp <- (nextmonthtemp*dtnew+mval[month]*dt)/(dtnew+dt)
+      deltatemp <- endtemp-starttemp
+      
+      # calculate vars for a,b,c coefficients in polynom y <- ax^2 +bx + c
+      d2t <- endt^2.0 - startt^2.0
+      d3t <- endt^3.0 - startt^3.0
+      
+      # Take a sheet of paper and try solve the polynom, well here is the outcome
+      polya <- (mval[month]*dt - deltatemp*d2t/dt/2.0 - starttemp*dt + deltatemp*startt) / (d3t/3.0 - d2t^2.0/dt/2.0 - dt*startt^2.0 + startt*d2t)
+      polyb <- deltatemp/dt - polya*(startt+endt)
+      polyc <- starttemp - polya*startt^2.0 - polyb*startt
+      
+      # calculate daily values with the polynom function
+      for (d in 1:ndaymonth[month]) {
+        day <- day + 1
+        dval[day] <- polya*(day)^2.0 + polyb*(day) + polyc
+      }
+      lastmonthtemp <- mval[month]
+    }
+    
+    # calculate monthly means after interpolation - not absolutely identical to input
+    mtempint <- rep(NA,nmonth)
+    day <- 0
+    for (m in 1:nmonth){
+      mtempint[m] <- 0.0
+      for (d in 1:ndaymonth[m]){
+        day <- day + 1
+        mtempint[m] <- mtempint[m]+dval[day]/(ndaymonth[m])
+      }
+    }
+    
+  } else if (method=="step"){
+    
+    dval[] <- rep( mval, times=ndaymonth )
+    
+  } else {
+    print( "Method (2nd argument) not valid." )
+  }
+  
+  return(dval) 
+  
+}
+
+
+##-----------------------------------------------------------
+## fills gaps (NAs) by (1.) linear interpolation, (2.) extending first/last to head/tail
+##-----------------------------------------------------------
+fill_gaps <- function( vec, is.prec = FALSE ){
+  
+  xvals <- seq(length(vec))
+  
+  if ( is.prec ){
+    ## assume precipitation = 0 where missing
+    if (any(is.na(vec))){
+      vec[ is.na(vec) ] <- 0.0
+    }
+    
+  } else {
+    ## linear approximation
+    if ( any(is.na(vec)) && any(!is.na(vec)) ){
+      vec <- approx( xvals, vec, xout=xvals )$y
+    }
+    
+    ## extend to missing in head and tail
+    if ( any(is.na(vec))  && any(!is.na(vec)) ){
+      for (idx in seq(length(vec))){
+        if ( any( is.na( tail( vec, n=idx ) ) ) && any( !is.na( tail( vec, n=(idx+1) ) ) ) ){
+          if (length(vec[ (length(vec)-idx) ])>0){
+            vec[ (length(vec)-idx+1):length(vec) ] <- vec[ (length(vec)-idx) ]
+          } else {
+            vec[ (length(vec)-idx+1):length(vec) ] <- NA
+          }
+          break
+        }
+      }
+    }  
+    
+  }
+  
+  return( vec )
+  
+}
