21360ff4f527b7d6e418f0e0bf4addda95295ed7
stineb <b.stocker@imperial.ac.uk>
2020-12-16 08:33:35 +0100

    implemented eact for calc_vpd()

--- R/calc_vpd.R
+++ R/calc_vpd.R
@@ -3,6 +3,7 @@
 #' xxx
 #'
 #' @param qair Air specific humidity (g g-1)
+#' @param eact Water vapour pressure (Pa)
 #' @param tc temperature, deg C
 #' @param tmin (optional) min daily air temp, deg C 
 #' @param tmax (optional) max daily air temp, deg C
@@ -13,7 +14,7 @@
 #' @return vapor pressure deficit (Pa)
 #' @export
 #' 
-calc_vpd <- function( qair=NA, tc=NA, tmin=NA, tmax=NA, patm=NA, elv=NA  ){
+calc_vpd <- function(qair=NA, eact=NA, tc=NA, tmin=NA, tmax=NA, patm=NA, elv=NA){
   ##-----------------------------------------------------------------------
   ## Ref:      Eq. 5.1, Abtew and Meleese (2013), Ch. 5 Vapor Pressure 
   ##           Calculation Methods, in Evaporation and Evapotranspiration: 
@@ -25,26 +26,28 @@ calc_vpd <- function( qair=NA, tc=NA, tmin=NA, tmax=NA, patm=NA, elv=NA  ){
   ##-----------------------------------------------------------------------
 
   ## calculate atmopheric pressure (Pa) assuming standard conditions at sea level (elv=0)
-  if (is.na(elv) && is.na(patm)) rlang::abort("calc_vpd(): Either patm or elv must be provided.")
-  patm <- ifelse(is.na(patm),
-                 calc_patm(elv),
-                 patm)
+  if (is.na(elv) && is.na(patm) && is.na(eact)) rlang::abort("calc_vpd(): Either patm or elv must be provided if eact is not given.")
+  if (is.na(eact)){
+    patm <- ifelse(is.na(patm),
+                   calc_patm(elv),
+                   patm)
+  }
   
   ## Calculate VPD as mean of VPD based on Tmin and VPD based on Tmax if they are availble.
   ## Otherwise, use just tc for calculating VPD.
   vpd <- ifelse(!is.na(tmin) && !is.na(tmax),
                 mean(
-                  calc_vpd_inst( qair=qair, tc=tmin, patm=patm), 
-                  calc_vpd_inst( qair=qair, tc=tmax, patm=patm)
+                  calc_vpd_inst( qair=qair, eact=eact, tc=tmin, patm=patm), 
+                  calc_vpd_inst( qair=qair, eact=eact, tc=tmax, patm=patm)
                   ),
-                calc_vpd_inst( qair=qair, tc=tc, patm=patm)
+                calc_vpd_inst( qair=qair, eact=eact, tc=tc, patm=patm)
                 )
   
   return( vpd )
   
 }
 
-calc_vpd_inst <- function( qair=NA, tc=NA, patm=NA, elv=NA  ){
+calc_vpd_inst <- function( qair=NA, eact=NA, tc=NA, patm=NA, elv=NA  ){
   ##-----------------------------------------------------------------------
   ## Ref:      Eq. 5.1, Abtew and Meleese (2013), Ch. 5 Vapor Pressure 
   ##           Calculation Methods, in Evaporation and Evapotranspiration: 
@@ -54,18 +57,20 @@ calc_vpd_inst <- function( qair=NA, tc=NA, patm=NA, elv=NA  ){
   ##                 tc = average daily air temperature, deg C
   ##                 eact  = actual vapor pressure, Pa
   ##-----------------------------------------------------------------------
-  kTo = 288.15   # base temperature, K (Prentice, unpublished)
-  kR  = 8.3143   # universal gas constant, J/mol/K (Allen, 1973)
-  kMv = 18.02    # molecular weight of water vapor, g/mol (Tsilingiris, 2008)
-  kMa = 28.963   # molecular weight of dry air, g/mol (Tsilingiris, 2008)
-  
-  ## calculate the mass mixing ratio of water vapor to dry air (dimensionless)
-  wair <- qair / (1 - qair)
-  
-  ## calculate water vapor pressure 
-  rv <- kR / kMv
-  rd <- kR / kMa
-  eact = patm * wair * rv / (rd + wair * rv)  
+  if (is.na(eact)){
+    kTo = 288.15   # base temperature, K (Prentice, unpublished)
+    kR  = 8.3143   # universal gas constant, J/mol/K (Allen, 1973)
+    kMv = 18.02    # molecular weight of water vapor, g/mol (Tsilingiris, 2008)
+    kMa = 28.963   # molecular weight of dry air, g/mol (Tsilingiris, 2008)
+    
+    ## calculate the mass mixing ratio of water vapor to dry air (dimensionless)
+    wair <- qair / (1 - qair)
+    
+    ## calculate water vapor pressure 
+    rv <- kR / kMv
+    rd <- kR / kMa
+    eact = patm * wair * rv / (rd + wair * rv)  
+  }
   
   ## calculate saturation water vapour pressure in Pa
   esat <- 611.0 * exp( (17.27 * tc)/(tc + 237.3) )
