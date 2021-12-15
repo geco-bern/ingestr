58e8746ca672c97d050a1b7cfb85c58395871cbd
khufkens <koen.hufkens@gmail.com>
2021-12-15 16:29:27 +0100

    unit test

---
+++ tests/testthat/test_ingestr.R
@@ -0,0 +1,53 @@
+# is the server reachable
+euler <- grepl('eu-', Sys.info()['nodename'])
+
+test_that("test init data frame", {
+  skip_on_cran()
+  df <- init_dates_dataframe(
+    2018,
+    2019,
+    noleap = TRUE,
+    timescale = "d"
+  )
+  
+  expect_type(df, "list")
+  
+})
+
+test_that("test MODIS LST download", {
+  skip_on_cran()
+  #skip_if_not(euler)
+  
+  source("R/get_settings.R")
+  source("R/ingest_bysite.R")
+  
+  df <- init_dates_dataframe(
+    2018,
+    2019,
+    noleap = TRUE,
+    timescale = "d"
+  )
+  
+  settings_modis <- get_settings_modis(
+    bundle            = "modis_lst_daily",
+    data_path         = tempdir(),
+    method_interpol   = "loess",
+    keep              = TRUE,
+    overwrite_raw     = TRUE,
+    overwrite_interpol= TRUE,
+    n_focal           = 0,
+    network           = "FLUXNET"
+  )
+  
+  df <- ingest_bysite(
+    sitename  = "CH-Lae",
+    source    = "modis",
+    year_start= 2018,
+    year_end  = 2019,
+    settings  = settings_modis,
+    verbose   = FALSE
+  )
+  
+  print(df)
+  
+})
