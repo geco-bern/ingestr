3379b582a4ec28ab539e3f01da7df5f3800ef198
khufkens <koen.hufkens@gmail.com>
2021-12-17 13:01:57 +0100

    test script

--- analysis/test_modis_downloads.R
+++ analysis/test_modis_downloads.R
@@ -1,8 +1,11 @@
+library(tidyverse)
+library(ingestr)
+
 settings_modis <- get_settings_modis(
   bundle            = "modis_lst_daily",
   data_path         = tempdir(),
-  method_interpol   = "sgfilter",
-  keep              = FALSE,
+  method_interpol   = "loess",
+  keep              = TRUE,
   overwrite_raw     = TRUE,
   overwrite_interpol= TRUE,
   n_focal           = 0,
@@ -13,9 +16,14 @@ df <- ingest_bysite(
   sitename  = "CH-Lae",
   source    = "modis",
   year_start= 2018,
-  year_end  = 2018,
+  year_end  = 2019,
   settings  = settings_modis,
   verbose   = FALSE
 )
 
-plot(df$date, df$lst)
+print(head(df))
+
+plot(df$date, df$spline, type = "l", col = "black", ylab = "LST")
+lines(df$date, df$loess, col = "red")
+points(df$date, df$sgfilter, col = "blue")
+lines(df$date, df$linear, col = "green")
