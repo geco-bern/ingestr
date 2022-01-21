20e8719d4c603e55dc95317e87e9dd00fbfce7c0
khufkens <koen.hufkens@gmail.com>
2022-01-21 15:38:21 +0100

    new unit tests

---
+++ tests/testthat/test_flux_formatting.R
@@ -0,0 +1,63 @@
+
+test_that("test HH data", {
+  skip_on_cran()
+  
+  library(tidyverse)
+  
+  siteinfo <- ingestr::siteinfo_fluxnet2015 %>%
+    filter(sitename == "FR-Pue")
+  
+  settings_fluxnet <- list(
+    getswc       = FALSE,
+    filter_ntdt  = TRUE,
+    threshold_GPP= 0.8,
+    remove_neg   = FALSE,
+    dir_hh = paste0(path.package("ingestr"), "/extdata/")
+  )
+  
+  df <- ingestr::ingest(
+    siteinfo,
+    source    = "fluxnet",
+    getvars   = list(
+      gpp = "GPP_NT_VUT_REF",
+      gpp_unc = "GPP_NT_VUT_SE"),
+    dir       = paste0(path.package("ingestr"), "/extdata/"),
+    settings  = settings_fluxnet,
+    timescale = "hh",
+    verbose = TRUE
+  )
+  
+  expect_type(df, "list")
+})
+
+test_that("test Daily data", {
+  skip_on_cran()
+  
+  library(tidyverse)
+  
+  siteinfo <- ingestr::siteinfo_fluxnet2015 %>%
+    filter(sitename == "FR-Pue")
+  
+  settings_fluxnet <- list(
+    getswc       = FALSE,
+    filter_ntdt  = TRUE,
+    threshold_GPP= 0.8,
+    remove_neg   = FALSE,
+    dir_hh = paste0(path.package("ingestr"), "/extdata/")
+  )
+  
+  df <- ingestr::ingest(
+    siteinfo,
+    source    = "fluxnet",
+    getvars   = list(
+        gpp = "GPP_NT_VUT_REF",
+        gpp_unc = "GPP_NT_VUT_SE"
+      ),
+    dir       = paste0(path.package("ingestr"), "/extdata/"), 
+    settings  = settings_fluxnet,
+    timescale = "d",
+    verbose = TRUE
+  )
+  
+  expect_type(df, "list")
+})
