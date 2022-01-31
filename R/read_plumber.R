0c6004f867cd9d9fa2649049140ee4c2dd985408
khufkens <koen.hufkens@gmail.com>
2022-01-31 15:19:32 +0100

    fluxnet unit conversions - on the fly

--- R/read_plumber.R
+++ R/read_plumber.R
@@ -7,6 +7,7 @@
 #'
 #' @param site fluxnet site name
 #' @param path path with plumber2 data (both flux and meteo data files)
+#' @param fluxnet_format convert to fluxnet formatting (TRUE/FALSE)
 #' @param meta_data return meta-data TRUE/FALSE
 #'
 #' @return data frame with merged meteo and flux data
@@ -15,8 +16,12 @@
 read_plumber <- function(
   site = "AT-Neu",
   path,
+  fluxnet_format = FALSE,
   meta_data = FALSE
 ){
+  
+  # CRAN settings
+  IGBP_veg_long <- NULL
   
   # list all files
   files <- list.files(
@@ -34,7 +39,7 @@ read_plumber <- function(
   # cut back on read times by only reading
   # flux data when extracting meta-data
   if (meta_data) {
-    files <- files[grepl(glob2rx("*_Flux.nc"), files)]
+    files <- files[grepl(utils::glob2rx("*_Flux.nc"), files)]
   }
   
   df <- lapply(files, function(file){
@@ -97,10 +102,88 @@ read_plumber <- function(
   # only return meta-data if requested
   # don't merge with met data
   if (!meta_data) {
-    all <- df
-  } else {
+    print("join")
     # combine met and flux data
     all <- left_join(df[[1]], df[[2]])
+  } else {
+    all <- df
+  }
+  
+  # format data as fluxnet compatible
+  if (fluxnet_format && !meta_data) {
+    # convert time, and only return
+    # FLUXNET formatted columns
+    
+    all <- all %>%
+      mutate(
+        TIMESTAMP_START = format(time, "%Y%m%d%H%M"),
+        TIMESTAMP_END = format(time + 30 * 60, "%Y%m%d%H%M")
+      )
+    
+    keys <- c(
+      # MICROMET
+      P = "Precip", # in mm -s
+      TA_F = "Tair",
+      SW_IN_F = "SWdown",
+      LW_IN_F = "LWdown",
+      VPD_F = "VPD",
+      WS_F = "Wind",
+      PA_F = "Psurf",
+      CO2_F = "CO2air",
+      # FLUXES
+      NETRAD = "Rnet",
+      USTAR = "Ustar",
+      SW_OUT = "SWup",
+      LE_F_MDS = "Qle",
+      LE_CORR = "Qle_cor",
+      H_F_MDS = "Qh",
+      H_CORR = "Qh_cor",
+      NEE_VUT_REF = "NEE",
+      GPP_VUT_REF = "GPP", # uncertain
+      GPP_VUT_REF_SE = "GPP_se", # uncertain
+      GPP_DT_VUT_REF = "GPP_DT",
+      GPP_DT_VUT_SE = "GPP_DT_se",
+      RECO_NT_VUT_REF = "Resp",
+      RECO_NT_VUT_SE = "Resp_se"
+    )
+    
+    # columns to select
+    loc <- names(keys[which(keys %in% colnames(all))])
+    
+    # loop over key and rename columns
+    for(i in 1:length(keys)) {
+      key <- keys[i]
+      if(key %in% colnames(all)) {
+        new_name <- as.character(names(key))
+        old_name <- as.character(key)
+        all <- all %>%
+          dplyr::rename(
+           !!new_name := !!old_name
+          )
+      }
+    }
+    
+  # return fluxnet selection
+  all  <- all %>%
+      select(
+        TIMESTAMP_START,
+        TIMESTAMP_END,
+        !!loc
+      )
+  
+  # remaining unit conversions
+  # K to C
+  # mm s-1 to mm (sum)
+  # other conversions
+  # https://github.com/aukkola/FluxnetLSM/blob/a256ffc894ed8182f9399afa1d83dea43ac36a95/R/Conversions.R
+  all <- all %>%
+    mutate(
+      P = P * 60 * 30, # mm/s to mm
+      TA_F = TA_F - 273.15, # K to C
+      PA_F = PA_F / 1000, # Pa to kPa
+      CO2_F = CO2_F # ppm to umolCO2 mol-1
+    )
+  
   }
   
   # return the merged file
---
+++ analysis/test_plumber_to_fluxnet_conversion.R
@@ -0,0 +1,19 @@
+# helping draft a script to convert the plumber data
+# to a fluxnet compatible format in order to process
+# data using the current ingestr routines rather
+# than writing another set of code for plumber data
+
+library(tidyverse)
+source("R/read_plumber.R")
+
+# read in demo data
+df <- read_plumber(
+  site = "AR-SLu",
+  path = "~/Desktop/flux_data/",
+  fluxnet_format = TRUE,
+  meta_data = FALSE
+)
+
+print(str(df))
+
+
