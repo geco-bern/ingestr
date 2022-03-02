7ee25ab6d3851a37ab389a6014c8d662bdcbe480
khufkens <koen.hufkens@gmail.com>
2022-03-02 15:13:17 +0100

    offline tests

--- analysis/test_flux_formatting.R
+++ analysis/test_flux_formatting.R
@@ -1,24 +1,24 @@
 library(tidyverse)
 library(ingestr)
 
-rm(df)
+#---- test PLUMBER based FLUXNET 2015 data ----
 
 settings_fluxnet <- list(
   getswc       = FALSE,
-  filter_ntdt  = TRUE,
+  filter_ntdt  = FALSE,
   threshold_GPP= 0.8,
-  remove_neg   = FALSE,
-  dir_hh = "~/Desktop/flux_data/"
+  remove_neg   = FALSE
 )
 
 siteinfo <- ingestr::siteinfo_fluxnet2015 %>%
-  filter(sitename == "AR-SLu")
+  filter(sitename == "AR-SLu") %>%
+  ungroup()
 
 df <- ingest(
   siteinfo,
   source    = "fluxnet",
   getvars   = list(
-    gpp = "GPP_NT_VUT_REF"
+    gpp = "GPP_VUT_REF"
     ),
   dir       = "~/Desktop/flux_data/", 
   settings  = settings_fluxnet,
@@ -27,3 +27,26 @@ df <- ingest(
 )
 
 print(df$data)
+
+#---- test true FLUXNET 2015 data ----
+
+siteinfo <- ingestr::siteinfo_fluxnet2015 %>%
+  filter(sitename == "FR-Pue")
+
+settings_fluxnet <- list(
+  getswc       = FALSE,
+  filter_ntdt  = TRUE,
+  threshold_GPP= 0.8,
+  remove_neg   = FALSE,
+  dir_hh = paste0(path.package("ingestr"), "/extdata/")
+)
+
+df <- ingestr::ingest(
+  siteinfo,
+  source    = "fluxnet",
+  getvars   = list(gpp = "GPP_NT_VUT_REF", gpp_unc = "GPP_NT_VUT_SE"),
+  dir       = paste0(path.package("ingestr"), "/extdata/"),
+  settings  = settings_fluxnet,
+  timescale = "hh",
+  verbose = TRUE
+)
